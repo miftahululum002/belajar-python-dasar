9ace843739bda7a3d5ea29021ccb2c212ebbb9c9
O027-MIFTAHUL ULUM,S.Pd.+Gr <ulumiftahul06@gmail.com>
2025-05-26 11:07:43 +0700

    variabel

---
+++ variable/variable_numbers.py
@@ -0,0 +1,9 @@
+# tipe data integer 
+x=5
+y=3
+z= 5+3
+
+
+# tipe data float 
+a = 7.5
+print(a)
---
+++ variable/variable_strings.py
@@ -0,0 +1,17 @@
+# Menggunakan single quote
+myFirstName = 'Miftahul'
+
+# Menggunakan double quote
+myLastName = "Ulum"
+
+# Penulisan Angka sebagai string
+myAge = "25"
+
+# Penulisan string multiline
+myHobby = """Hobi saya adalah
+bersepeda dan bermain game.
+"""
+
+# Mencetak 2 variabel string dengan perintah print
+print(myFirstName + " " + myLastName)
+print("Umur saya " + myAge)
